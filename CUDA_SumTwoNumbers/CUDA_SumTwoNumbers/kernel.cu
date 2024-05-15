a7b9b346f6e682ab4e15be5f79f20ae1cd44c733
SadiqParvizoglu <150825954+SadiqParvizoglu@users.noreply.github.com>
2024-05-15 23:01:31 +0400

    Add files via upload

---
+++ CUDA_SumTwoNumbers/CUDA_SumTwoNumbers/kernel.cu
@@ -0,0 +1,21 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__global__ void sumTwoNumbers(int a,int b,int *c)
+{
+    *c = a + b;
+}
+
+int main()
+{
+    int host_c;
+    int* dev_c;
+    cudaMalloc((void**)&dev_c, sizeof(int));
+    sumTwoNumbers << <1, 1 >> > (17, 24, dev_c);
+    cudaMemcpy(&host_c, dev_c, sizeof(int),cudaMemcpyDeviceToHost);
+    printf("17 + 24 = %d\n", host_c);
+    cudaFree(dev_c);
+    return 0;
+}
