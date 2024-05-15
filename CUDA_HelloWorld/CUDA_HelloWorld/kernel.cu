55584af97b42266184fe327faf15108bb4b3739e
SadiqParvizoglu <150825954+SadiqParvizoglu@users.noreply.github.com>
2024-05-15 23:00:30 +0400

    Add files via upload

---
+++ CUDA_HelloWorld/CUDA_HelloWorld/kernel.cu
@@ -0,0 +1,15 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__global__ void HelloWorld()
+{
+    printf("Hello World, %d, %d\n",blockIdx.x,threadIdx.x);
+}
+
+int main()
+{
+    HelloWorld << <1, 1 >> > ();
+    return 0;
+}
