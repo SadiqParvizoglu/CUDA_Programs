8b5a39d1d4ac744c2c32a0c8d8303c41c090d0e6
SadiqParvizoglu <150825954+SadiqParvizoglu@users.noreply.github.com>
2024-05-15 23:11:43 +0400

    Add files via upload

---
+++ CUDA_Parallelization_Threads/CUDA_Parallelization_Threads/kernel.cu
@@ -0,0 +1,18 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__global__ void kernel()
+{	
+	int tid = threadIdx.x;
+	printf("Thread number %d\n", tid);
+}
+
+int main(void)
+{
+	int* dev_a;
+	cudaMalloc((void**)&dev_a, sizeof(int));
+	kernel << <1, 10 >> > ();
+	cudaFree(dev_a);
+	return 0;
+}
