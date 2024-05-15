7039fddff02cf4377b34e47f65d3d5ad3f2f8d2c
SadiqParvizoglu <150825954+SadiqParvizoglu@users.noreply.github.com>
2024-05-15 23:12:34 +0400

    Add files via upload

---
+++ CUDA_Parallelization_BlocksAndThreads/CUDA_Parallelization_BlocksAndThreads/kernel.cu
@@ -0,0 +1,20 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+const int ThreadCount=15;
+const int BlockCount=10;
+
+__global__ void kernel()
+{
+    int tid_block = blockIdx.x;
+    int tid_thread = threadIdx.x;
+    printf("Block number %d. Thread number %d\n",tid_block, tid_thread);
+}
+int main(void)
+{
+	int* dev_a;
+	cudaMalloc((void**)&dev_a, sizeof(int));
+	kernel << <BlockCount, ThreadCount>> > ();
+	cudaFree(dev_a);
+	return 0;
+}
